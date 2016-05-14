b6b10863731cd0bfcdc1d8fa6ddb2b9684dd6945
kevenfeng <kevenfeng@dengtacj.com>
2016-05-14 09:18:19 +0800

    首次提交

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ class DotaClub extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit 11index.ios.js
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
